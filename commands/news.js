c4c614528531701577e332f7fb2c6a834f2c3c37
mixingamesYT <fiebep@gmail.com>
2020-01-18 16:09:46 +0100

    Update news.js

--- commands/news.js
+++ commands/news.js
@@ -6,8 +6,19 @@ module.exports.run = async (bot, message, args) => {
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have the permissions to use that command").then(msg => msg.delete(3000));
 
-    if(!args[0]) return message.channel.send("Use: ?news [message]").then(msg => msg.delete(3000));
+    var split = "||";
 
+    if(!args[0] == null){
+
+        var useMessage = new discord.RichEmbed()
+        .setTitle("Use:")
+        .setColor("00ee00")
+        .setDescription(`?news Title ${split} Color ${split} Channel ${split} Message`);
+
+        return message.channel.send(useMessage);
+
+    }
+    
     try{
         var newsMessage = args.join(" ");
 
