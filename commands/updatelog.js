ca55de2a3ac9207f4996ff51fc8583a6e424c051
mixingamesYT <59974381+mixingamesYT@users.noreply.github.com>
2020-01-19 16:36:42 +0100

    Update updatelog.js

--- commands/updatelog.js
+++ commands/updatelog.js
@@ -4,9 +4,13 @@ module.exports.run = async (bot, message, args) => {
 
     message.channel.bulkDelete("1");
 
-    if (args[0] == 1.1){
+    if (args[0] == "1.1"){
 
         return message.channel.send("**THE ADMIN UPDATE** \n\n1) Added join roles and message \n2) Added the news command **(only for admin)** \n3) Added ![EN]warn + warn system **(only for admin)** \n4) Added the ![EN]tempmute command **(only for admin)**");
+
+    } else if(args[0] == "1.1.1"){
+
+        return message.channel.send("**THE NEWS UPDATE** \n\nGreat change of the news command! \nHow you use it now: \n?news Title // Color // Channel // Message \nStill only for Admin!")
 
     } else{
         return message.channel.send("**Error:** there are only updatelogs beginning from 1.1 or this update doesn't exist").then(msg => msg.delete(5000));
