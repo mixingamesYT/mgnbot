4b50b37e7bd5a3b5e448472ee7642a9280dfd6cf
mixingamesYT <fiebep@gmail.com>
2020-01-19 20:35:27 +0100

    Update updatelog.js

--- commands/updatelog.js
+++ commands/updatelog.js
@@ -6,11 +6,11 @@ module.exports.run = async (bot, message, args) => {
 
     if (args[0] == "1.1"){
 
-        return message.channel.send("**THE ADMIN UPDATE** \n\n1) Added the news command (Pre-release) **(only for admin)** \n2) Added ?warn + warn system **(only for admin)** \n3) Added the ?tempmute command **(only for admin)**");
+        return message.channel.send("**THE ADMIN UPDATE** \n\n1) Added the news command (Pre-release) **(only for admin)** \n2) Added ?warn + warn system **(only for admin)** \n3) Added the ?tempmute command **(only for admin)**\n");
 
     } else if(args[0] == "1.1.1"){
 
-        return message.channel.send("**THE NEWS UPDATE** \n\nRelease of the news command! \nHow you use it : \n?news Title // Color // Channel // Message \nStill only for Admin!")
+        return message.channel.send("**THE NEWS UPDATE** \n\nRelease of the news command! \nHow you use it : \n?news Title // Color // Channel // Message \nStill only for Admin!\n")
 
     } else{
         return message.channel.send("**Error:** there are only updatelogs beginning from 1.1 or this update doesn't exist").then(msg => msg.delete(5000));
