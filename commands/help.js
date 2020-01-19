136ad4462c12c749a2e127c6dc2c6f7b658d94e6
mixingamesYT <59974381+mixingamesYT@users.noreply.github.com>
2020-01-19 16:35:19 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
 
     try{
 
-var text = "**AS bot English** \n\n**__Commands:__** \n__?help__ - You will see this menu \n__?hello__ - The bot says hello back \n__?bot__ - Crain information abou this bot \n__?server__ - Crain information about this server \n__?ping__ - crain the ping of the bot \n__?clear__ - Delete messages **(Admin)** \n__?kick__ - Kick anyone **(Admin)** \n__?ban__ - Ban anyone **(Admin)** \n__?news__ - Send a message to the channel news **(Admin)** \n__?warn__ - Warn anyone (4 warns = ban) **(Admin)** \n__?tempmute__ - temporarily mute anyone **(Admin)** \n**!** Means that the commend doesn't work very well or that it's under construction";
+var text = "**AS bot English** \n\n**__Commands:__** \n__?help__ - You will see this menu \n__?hello__ - The bot says hello back \n__?bot__ - Crain information abou this bot \n__?server__ - Crain information about this server \n__?ping__ - crain the ping of the bot \n__?clear__ - Delete messages **(Admin)** \n__?kick__ - Kick anyone **(Admin)** \n__?ban__ - Ban anyone **(Admin)** \n__?news__ - Send a message to the channel news **(Admin)** \n__?warn__ - Warn anyone (4 warns = ban) **(Admin)** \n__?tempmute__ - temporarily mute anyone **(Admin)** \n__?news__ Title // Color // Channel // Message - Send a news message **(Admin)**\n**!** Means that the commend doesn't work very well or that it's under construction";
 
         message.author.send(text);
 
