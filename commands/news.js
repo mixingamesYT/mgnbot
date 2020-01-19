556666df1882b8a71194b08f307cb848f87637eb
mixingamesYT <fiebep@gmail.com>
2020-01-19 16:15:17 +0100

    Update news.js

--- commands/news.js
+++ commands/news.js
@@ -1,27 +1,50 @@
 const discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async(bot, message, args) =>{
 
     message.channel.bulkDelete("1");
 
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have the permissions to use that command").then(msg => msg.delete(3000));
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the permission to use that command!").then(msg => msg.delete(5000));;
 
-    var split = "||";
+    var splitser = "//"
 
-    if(!args[0] == null){
-        
-        try{
-            var useMessage = new discord.RichEmbed
-                .setTitle("Use:")
-                .setColor("00ee00")
-                .setDescription(`!news Title ${split} Color ${split} Channel ${split} Message`);
+    if(args[0] == null) {
 
-            return message.channel.send(useMessage).then(msg => msg.delete(5000));
-        } catch (error){
-            console.log(error);
-        }
+        var useMessage = new discord.RichEmbed()
+            .setTitle("Use")
+            .setColor("00ee00")
+            .setDescription(`Make a news message by using: \n!news Title ${splitser} Color ${splitser} Channel ${splitser} Message`);
+
+        return message.channel.send(useMessage).then(msg => msg.delete(10000));
 
     }
+
+    args = args.join(" ").split(splitser);
+
+    if(args[1] == undefined) args[1] = "#eeeeee";
+    if(args[2] == undefined) args[2] = "news";
+
+    var options = {
+
+        titel: args[0] || "News message:",
+        kleur: args[1].trim(),
+        kanaal: args[2].trim(),
+        bericht: args[3] || "No news message"
+
+    }
+
+    var writer = message.author;
+
+    var newsMessage = new discord.RichEmbed()
+        .setTitle(options.titel)
+        .setColor(options.kleur)
+        .setDescription(`Message by ${writer} \n\n${options.bericht} \n`)
+        .setTimestamp();
+
+    var newsChannel = message.guild.channels.find(`name`, options.kanaal);
+    if(!newsChannel) return message.channel.send("Didn't find the channel").then(msg => msg.delete(5000));
+
+    newsChannel.send(newsMessage);
 
 }
 
