02beba6a289efbb203fb2db24b1c6905857300cd
mixingamesYT <fiebep@gmail.com>
2020-01-20 15:08:47 +0100

    Update bot.js

--- commands/bot.js
+++ commands/bot.js
@@ -7,12 +7,12 @@ module.exports.run = async (bot, message, args) => {
     var botIcon = bot.user.displayAvatarURL
 
     var botEmbed = new discord.RichEmbed()
-        .setDescription("About this bot")
+        .setTitle("**About this bot**")
         .setColor("#29e53f")
         .setThumbnail(botIcon)
         .addField("Name of this bot:", "AllesServer English")
-        .addField("Version:", "1.1")
-        .addField("New features", "Added new commands! \n(type ?updatelog 1.1 for the whole updatelog of this version)")
+        .addField("Version:", "1.1.1")
+        .addField("New features", "Added new commands! \n(type ?updatelog 1.1.1 for the whole updatelog of this version)")
         .addField("Bot created at", bot.user.createdAt);
 
     return message.channel.send(botEmbed);
